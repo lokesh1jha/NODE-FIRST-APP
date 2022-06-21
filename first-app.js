4acdb6d65785cfd112eb106726e2e53a50fb473c
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-06-21 21:15:49 +0530

    Second Commit
    arrow function to return product of 2 number
    Added a student object

--- first-app.js
+++ first-app.js
@@ -3,3 +3,15 @@ const fs = require('fs'); //imported file system and stored in const fs
 
 //this system will provide facility to write in file i.e on localstorage 
 fs.writeFileSync("hello.txt", "Hello from node.js") // file pat with name, fie contenet
+
+let productOfTwo = (a,b) => a*b;
+console.log(productOfTwo(2,3));
+
+//student object
+const student = {
+    name:"Lokesh",
+    rollno: 64,
+    marks() { console.log(`${this.name} Scored 90 % `) } 
+}
+
+student.marks();
