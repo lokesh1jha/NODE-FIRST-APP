27ccfb6a264b6be74fe6f96c37155e022cc9c52e
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-06-23 20:49:10 +0530

    buffer, res.on, eventListner  app.js
    Learnt about how data from web page can be stored in a file
    Data is taken in chunks and later all are combined to make the real meaningful data

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    if (req.url == '/') { //check the URL of the current request
+    // console.log(req);
+    const url = req.url;
+    const method = req.method;
+
+    if (url == '/') { //check the URL of the current request
         
         // set response header
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
         // set response content    
-        res.write('<html><body><p>Lokesh Kumar Jha</p></body></html>');
-        res.end();
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();//return is used so that further code should not be run
     }
 
+    //This is the way when a click on web page can trigger codes to run
+    //Here code is to write DUMMY in a file named message.txt
+    /*if(url === '/message' && method === 'POST'){
+            fs.writeFileSync('message.txt', 'DUMMY');
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+    }*/
+
+    //How can we write data from webpage to files on system
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {   // res.on -> 'on' is listning from webpage ; 'data' -> 'on' will listen to the data send to server
+            console.log(chunk); //to see how chunk of data looks
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message); 
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+    }
     // process.exit();  //it will close the server from running
     //Then you will not be able to reach your web page
 })
